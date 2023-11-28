536a7825ab4de8fc33c925fb90b705250093ad26
Sunwoo Song <sunwsong@c1r5s3.42seoul.kr>
2023-11-28 21:16:07 +0900

    modify: not socket working

--- client.c
+++ client.c
@@ -11,42 +11,78 @@
 
 #define BUF_SIZE 1024
 
+void err(char* str)
+{
+    printf("%s\n", str);
+    exit(1);
+}
+
+void* thread_input_to_socket(void* sock_ptr) {
+    int sock = *(int*)sock_ptr;
+    char command[BUF_SIZE];
+
+    while (1) {
+        memset(command, 0, sizeof(command));
+        read(STDIN_FILENO, command, sizeof(command));
+        //scanf("%s", command);
+        printf("command: %s", command);
+        if (write(sock, command, strlen(command)) == -1)
+            err("socket write error");
+    }
+
+    return NULL;
+}
+
+void* thread_socket_to_output(void* sock_ptr) {
+    int sock = *(int*)sock_ptr;
+    char message[BUF_SIZE];
+
+    while (1) {
+        memset(message, 0, sizeof(message));
+        printf("output socket waiting for read ... \n");
+        int len = read(sock, message, BUF_SIZE - 1);
+        if (len == -1) {
+            printf("read error\n");
+            break;
+        }
+        printf("message: %s", message);
+    }
+
+    return NULL;
+}
+
 int main(int ac, char** av)
 {
     int sock;
     char message[BUF_SIZE];
     struct sockaddr_in serv_addr;
+    pthread_t input_thread, output_thread;
 
-    if (ac != 3)
+    if (ac != 3) {
+        printf("Usage : %s <IP> <port>\n", av[0]);
         return 1;
+    }
 
-    sock = socket(PF_INET, SOCK_DGRAM, 0);
+    sock = socket(PF_INET, SOCK_STREAM, 0);
     if (sock == -1)
-        return 1;
+        err("socket() error");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(av[1]);
+    serv_addr.sin_addr.s_addr = inet_addr(av[1]);
     serv_addr.sin_port = htons(atoi(av[2]));
 
     if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        return 1;
+        err("connect() error");
 
-    while (1) {
-        memset(message, 0, sizeof(message));
-        scanf("%s", message);
-        if (message[BUF_SIZE - 1] != 0)
-            break;
+    printf("connect success\n");
+    pthread_create(&input_thread, NULL, thread_input_to_socket, &sock);
+    // close(STDIN_FILENO);
+    pthread_create(&output_thread, NULL, thread_socket_to_output, &sock);
+    // close(STDOUT_FILENO);
 
-        if (write(sock, message, strlen(message)) == -1)
-            break;
-
-        int len = read(sock, message, BUF_SIZE - 1);
-        if (len == -1)
-            break;
-        message[len] = 0;
-        printf("%s\n", message);
-    }
+    pthread_join(input_thread, NULL);
+    pthread_join(output_thread, NULL);
     close(sock);
     return 0;
 }
--- server.c
+++ server.c
@@ -14,15 +14,33 @@
 
 pthread_t clients[MAX_CLIENT];
 
-void* shell(void* client_num)
+void err(char* str)
 {
+    printf("%s\n", str);
+    exit(1);
+}
+
+void* shell(void* val)
+{
+    int clnt_sock = *(int*)val;
     pid_t pid = fork();
-    if (pid == 0) {
+    if (pid < 0)
+        err("fork error");
+    else if (pid == 0) {
+        dup2(clnt_sock, STDIN_FILENO);
+        dup2(clnt_sock, STDOUT_FILENO);
+        // close(STDIN_FILENO);
+        // close(STDOUT_FILENO);
+
         system("cd; /bin/bash");
+        exit(0);
     }
     else {
+        close(clnt_sock);
         wait(&pid);
     }
+
+    return NULL;
 }
 
 int main(int ac, char** av)
@@ -34,24 +52,31 @@ int main(int ac, char** av)
     struct sockaddr_in serv_addr, clnt_addr;
     socklen_t clnt_addr_size;
 
-    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(atoi(av[1]));
 
     if (bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
-        return 1;
+        err("bind error\n");
     if (listen(serv_sock, 5) == -1)
-        return 1;
+        err("listen error\n");
 
     while (1) {
         clnt_addr_size = sizeof(clnt_addr);
+        printf("waiting accept\n");
         clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+        printf("accepted\n");
         if (clnt_sock == -1)
             continue;
 
-        pthread_create(clients[client_num], NULL, shell, client_num);
+        pthread_create(&clients[client_num], NULL, shell, &client_num);
         ++client_num;
     }
+
+    for (int i = 0; i < client_num; i++)
+        pthread_join(clients[i], NULL);
+
+    return 0;
 }
